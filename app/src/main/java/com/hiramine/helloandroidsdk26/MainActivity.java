33a33190c40578e836fb2a42bd115528467e0f1f
Nobuki HIRAMINE <nobuki@hiramine.com>
2018-06-02 22:23:18 +0900

    First commit

---
+++ app/src/main/java/com/hiramine/helloandroidsdk26/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hiramine.helloandroidsdk26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+	}
+}
